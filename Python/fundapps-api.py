a3a0b06a093a1e22d2d8bc742b2b500c5cbd2f1b
Will Smith <will@fundapps.co>
2021-02-26 17:33:39 +0000

    fundapps-api.py Python 3.5 compatible

--- Python/fundapps-api.py
+++ Python/fundapps-api.py
@@ -18,12 +18,12 @@ positions_file      = '' # XML or ZIP file
 base_url = f'https://{client_environment}-api.fundapps.co'
 auth = HTTPBasicAuth(api_user, api_password)
 files = {'file': (positions_file, open(positions_file, 'rb'), 'text/XML')}
-r = req.post(f'{base_url}/v1/expost/check', auth=auth, files=files)
+r = req.post('{}/v1/expost/check'.format(base_url), auth=auth, files=files)
 
 if r.status_code == 202:
     print('File accepted.')
     xml_response = ElementTree.fromstring(r.content)
-    result_tracking_url = f'{base_url}{xml_response[0].text}'
+    result_tracking_url = '{}{}'.format(base_url, xml_response[0].text)
 
     r = req.get(result_tracking_url, auth=auth)
     
@@ -34,4 +34,4 @@ if r.status_code == 202:
         
     print('Upload complete. Result XML:\n', r.text)
 else:
-    print(f'Unexpected error response from FundApps API. HTTP Status code:{r.status_code}, reason:{r.reason}')
+    print('Unexpected error response from FundApps API. HTTP Status code:{}, reason:{}'.format(r.status_code, r.reason))
